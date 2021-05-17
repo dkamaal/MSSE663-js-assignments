3581426e65815be2f8a718bf9c50c6a2f4dfc6b2
Danish <dkamaal@regis.edu>
2021-05-16 18:46:59 -0600

    Week 2 Assignment

--- week2/arrays-and-loops.js
+++ week2/arrays-and-loops.js
@@ -1,14 +1,22 @@
 // #1 Take an array of numbers and make them strings
 function stringItUp(arr) {
-	// your code here
+	var newStringArr = arr.map((value) => {
+		return value.toString();
+	});
+	return newStringArr;
 }
-
 console.log(stringItUp([2, 5, 100]));
 // output: ["2", "5", "100"]
 
+
 // #2 Make an array of strings of the names
 function namesOnly(arr) {
-	// your code here
+
+	var fullName = [];
+	for (let value of arr)
+		fullName.push(value.name);
+	return fullName;
+
 }
 
 console.log(
@@ -39,7 +47,8 @@ console.log(
 
 // #3 Filter the output array from #2.
 function oldOnesOnly(arr) {
-	// your code here
+	var result = arr.filter(actor => actor.name === 'Angelina Jolie' || actor.name === 'Bob Ziroll').map(actor => actor.age);
+	return result;
 }
 console.log(
 	oldOnesOnly([
@@ -66,3 +75,10 @@ console.log(
 	])
 );
 // output: [80, 100]
+
+/*
+References
+Worrell, M. (n.d) MSSE 663 - JS-TS Basics I. Powerpoint Presentation. World Class. College of Computer & Information Sciences. Regis University
+Mastering JS (2020) Retrieved from https://masteringjs.io/tutorials/fundamentals/filter-array-of-objects
+Stackoverflow (2019) Retrieved from https://stackoverflow.com/questions/55993277/make-an-array-of-strings-of-the-names
+*/
--- week2/object-literal.js
+++ week2/object-literal.js
@@ -3,6 +3,49 @@
  * #2 Give it 3 unique properties
  * #3 Give it at least 2 behaviours/methods of your choice
  * #4 Calls the object's method(s) and output a result of your choice to the console.
+ */
+var monkey = {
+    fruit: "Banana",
+    live: "Tree",
+    age: "5",
+    climb: function(){
+        return "Jumps on " + this.live;
+    },
+    eat: function() {
+        return "Eats " + this.fruit + " cake on " + this.age + "th Birthday";
+    }
+};
+console.log(monkey.climb());
+console.log(monkey.eat());
+/*
  * #5 Convert the object literal to an Class
  * #6 Then call its method and output to the console.
  */
+
+let Monkey = class {
+    constructor(age, live, fruit){
+        this.age = age;
+        this.live = live;
+        this.fruit = fruit;
+    }
+    climb() {
+        return "Jumps on " + this.live;
+    }
+    eat() {
+        return "Eats " + this.fruit + " cake on " + this.age + "th Birthday.";
+    }
+
+};
+
+let funnyMonkey = new Monkey(6, "Mango Tree", "Mango");
+let happyMonkey = new Monkey(4, "Banana Tree", "Banana");
+
+console.log("Funny Monkey " + funnyMonkey.climb() + " and " + funnyMonkey.eat());
+console.log("Happy Monkey " + happyMonkey.climb() + " and " + happyMonkey.eat());
+
+/*
+References
+Worrell, M. (n.d) MSSE 663 - JS-TS Basics I. Powerpoint Presentation. World Class. College of Computer & Information Sciences. Regis University
+W3SCHOOLS (2021). Retrieved from https://www.w3schools.com/js/js_objects.asp
+W3SCHOOLS (2021). Retrieved from https://www.w3schools.com/js/js_classes.asp
+*/
